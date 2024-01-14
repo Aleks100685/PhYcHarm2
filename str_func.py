38a29fa69c6a1fbb3afcf08e2a46b3c42de5dc55
Aleks100685 <155984443+Aleks100685@users.noreply.github.com>
2024-01-14 14:00:19 +0700

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
 def capitalize_string(input_string=str):
+    """
+    This function takes an input string and returns it in uppercase.
+    """
     return input_string.upper()
 result = capitalize_string('Dsdsds')
 print(result)
